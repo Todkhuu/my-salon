15e8be8451054683edf1f37df48cdfeb0adbe794
Todkhuu <todoo.zorigtbaatar@gmail.com>
2025-04-24 06:49:18 +0800

    Тодорхой тайлбар оруулна

--- src/app/api/(auth)/me/route.ts
+++ src/app/api/(auth)/me/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
 import { getUserFromCookie } from "@/lib/auth";
+import { connectMongoDb } from "@/server/database/db";
 
 export async function GET() {
+  connectMongoDb();
   const user = await getUserFromCookie();
 
   if (!user) {
